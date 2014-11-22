25b4c495a5d4603a4e58be7ba3af06c1000f6908
Alexander Ellingsen <alexellingsen@gmail.com>
2014-11-22 08:01:07 +0100

    Disable ControlBox for options form

--- BSA Browser/OptionsForm.Designer.cs
+++ BSA Browser/OptionsForm.Designer.cs
@@ -221,6 +221,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(584, 361);
+            this.ControlBox = false;
             this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnOK);
