fa9352dfa2886bb615b01772d8601692ed9fde9d
Alexander Ellingsen <alexellingsen@gmail.com>
2016-07-11 02:23:37 +0200

    Revert "Add some const. Part of a bigger "project""
    This reverts commit f43d72675ffca5bacc9cec51226bc8faea8d6ee8.

--- BSA Browser/BSABrowser.cs
+++ BSA Browser/BSABrowser.cs
@@ -599,12 +599,6 @@ namespace BSA_Browser
 
         #endregion
 
-        public const int MW_BSAHEADER_FILEID = 0x00000100; //!< Magic for Morrowind BSA
-        public const int OB_BSAHEADER_FILEID = 0x00415342; //!< Magic for Oblivion BSA, the literal string "BSA\0".
-
-        public const int OB_BSAHEADER_VERSION = 0x67; //!< Version number of an Oblivion BSA
-        public const int F3_BSAHEADER_VERSION = 0x68; //!< Version number of a Fallout 3 BSA
-
         /// <summary>
         /// Opens the given BSA archive, adding it to the TreeView and making it browsable.
         /// </summary>
@@ -621,7 +615,7 @@ namespace BSA_Browser
                 //if(Program.ReadCString(br)!="BSA") throw new fommException("File was not a valid BSA archive");
                 uint type = newNode.BinaryReader.ReadUInt32();
                 StringBuilder sb = new StringBuilder(64);
-                if (type != OB_BSAHEADER_FILEID && type != MW_BSAHEADER_FILEID)
+                if (type != 0x00415342 && type != 0x00000100)
                 {
                     //Might be a fallout 2 dat
                     newNode.BinaryReader.BaseStream.Position = newNode.BinaryReader.BaseStream.Length - 8;
@@ -686,7 +680,7 @@ namespace BSA_Browser
                 {
                     int version = newNode.BinaryReader.ReadInt32();
 
-                    if (version != OB_BSAHEADER_VERSION && version != F3_BSAHEADER_VERSION)
+                    if (version != 0x67 && version != 0x68)
                     {
                         if (MessageBox.Show("This BSA archive has an unknown version number.\n" +
                                             "Attempt to open anyway?", "Warning", MessageBoxButtons.YesNo) != DialogResult.Yes)
@@ -699,7 +693,7 @@ namespace BSA_Browser
                     newNode.BinaryReader.BaseStream.Position += 4;
                     uint flags = newNode.BinaryReader.ReadUInt32();
                     newNode.Compressed = ((flags & 0x004) > 0);
-                    newNode.ContainsFileNameBlobs = ((flags & 0x100) > 0 && version == OB_BSAHEADER_VERSION);
+                    newNode.ContainsFileNameBlobs = ((flags & 0x100) > 0 && version == 0x68);
                     int FolderCount = newNode.BinaryReader.ReadInt32();
                     int FileCount = newNode.BinaryReader.ReadInt32();
                     newNode.BinaryReader.BaseStream.Position += 12;
