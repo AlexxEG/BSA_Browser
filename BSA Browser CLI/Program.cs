e1bd2c3b70b7f48ce9d1b253fa1fbf6bb0404b6f
Alexander Ellingsen <alexellingsen@gmail.com>
2019-08-10 18:30:18 +0200

    CLI: Add --encoding & -i (ignore errors) flags, accept slashes and hyphens, set proper exit codes, update --help text

--- BSA Browser CLI/Program.cs
+++ BSA Browser CLI/Program.cs
@@ -29,6 +29,7 @@ namespace BSA_Browser_CLI
         public bool Help { get; private set; }
         public bool List { get; private set; }
         public bool ATI { get; private set; }
+        public bool IgnoreErrors { get; private set; }
 
         public Filtering Filtering { get; private set; } = Filtering.None;
         public ListOptions ListOptions { get; private set; } = ListOptions.None;
@@ -42,23 +43,36 @@ namespace BSA_Browser_CLI
 
         public Arguments(string[] args)
         {
-            List<string> input = new List<string>();
+            var input = new List<string>();
 
             for (int i = 0; i < args.Length; i++)
             {
                 string arg = args[i];
 
-                if (arg.StartsWith("/"))
+                if (arg.StartsWith("/") || arg.StartsWith("-") || arg.StartsWith("--"))
                 {
-                    switch (arg.ToLower().Split(':')[0])
+                    switch (arg.ToLower().Split(':', '=')[0])
                     {
                         case "/?":
+                        case "-h":
+                        case "--help":
                             this.Help = true;
                             break;
                         case "/e":
+                        case "-e":
                             this.Extract = true;
                             break;
+                        case "/f":
+                        case "-f":
+                            this.Filtering = Filtering.Simple;
+                            this.FilterString = args[++i];
+                            break;
+                        case "/i":
+                        case "-i":
+                            this.IgnoreErrors = true;
+                            break;
                         case "/l":
+                        case "-l":
                             this.List = true;
 
                             char[] options = arg.Split(':').Last().ToLower().ToCharArray();
@@ -69,18 +83,18 @@ namespace BSA_Browser_CLI
 
                             break;
                         case "/ati":
+                        case "--ati":
                             this.ATI = true;
                             break;
-                        case "/f":
-                            this.Filtering = Filtering.Simple;
-                            this.FilterString = args[++i];
-                            break;
                         case "/regex":
+                        case "--regex":
                             this.Filtering = Filtering.Regex;
                             this.FilterString = args[++i];
                             break;
                         case "/enc":
+                        case "--enc":
                         case "/encoding":
+                        case "--encoding":
                             this.Encoding = this.ParseEncoding(args[++i]);
                             break;
                         default:
@@ -128,6 +142,11 @@ namespace BSA_Browser_CLI
 
     class Program
     {
+        private const int ERROR_INVALID_FUNCTION = 1;
+        private const int ERROR_FILE_NOT_FOUND = 2;
+        private const int ERROR_PATH_NOT_FOUND = 3;
+        private const int ERROR_BAD_ARGUMENTS = 160;
+
         static Arguments _arguments;
         static Regex _regex;
         static WildcardPattern _pattern;
@@ -146,6 +165,7 @@ namespace BSA_Browser_CLI
             if (_arguments.Inputs.Count == 0)
             {
                 Console.WriteLine("No input file(s) found");
+                Environment.ExitCode = ERROR_FILE_NOT_FOUND;
                 goto exit;
             }
 
@@ -167,6 +187,7 @@ namespace BSA_Browser_CLI
                     catch
                     {
                         Console.WriteLine("Invalid regex filter string");
+                        Environment.ExitCode = ERROR_BAD_ARGUMENTS;
                         goto exit;
                     }
                 }
@@ -174,7 +195,16 @@ namespace BSA_Browser_CLI
 
             if (_arguments.List || (!_arguments.List && !_arguments.Extract && !_arguments.Help))
             {
-                PrintFileList(_arguments.Inputs.ToList(), _arguments.ListOptions);
+                try
+                {
+                    PrintFileList(_arguments.Inputs.ToList(), _arguments.ListOptions);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("An error occured opening archive:");
+                    Console.WriteLine(ex.Message);
+                    Environment.ExitCode = ERROR_INVALID_FUNCTION;
+                }
             }
 
             if (_arguments.Extract)
@@ -182,10 +212,20 @@ namespace BSA_Browser_CLI
                 if (string.IsNullOrEmpty(_arguments.Destination) || !Directory.Exists(_arguments.Destination))
                 {
                     Console.WriteLine($"Destination \'{_arguments.Destination}\' not found!");
+                    Environment.ExitCode = ERROR_PATH_NOT_FOUND;
                     goto exit;
                 }
 
-                ExtractFiles(_arguments.Inputs.ToList(), _arguments.ATI, _arguments.Destination);
+                try
+                {
+                    ExtractFiles(_arguments.Inputs.ToList(), _arguments.ATI, _arguments.Destination);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("An error occured opening archive:");
+                    Console.WriteLine(ex.Message);
+                    Environment.ExitCode = ERROR_INVALID_FUNCTION;
+                }
             }
 
         exit:;
@@ -196,85 +236,93 @@ namespace BSA_Browser_CLI
 #endif
         }
 
-        static void ExtractFiles(List<string> files, bool ati, string destination)
+        static void ExtractFiles(List<string> archives, bool ati, string destination)
         {
-            files.ForEach(file =>
+            archives.ForEach(archivePath =>
             {
-                var archive = OpenArchive(file, ati);
+                Archive archive = null;
 
-                if (archive == null) { }
-                else
+                try
                 {
-                    int count = 0;
-                    int total = archive.Files.Count(x => Filter(x.FullPath));
-                    int line = -1;
-                    int prevLength = 0;
+                    archive = OpenArchive(archivePath, ati);
+                }
+                catch (Exception ex)
+                {
+                    if (!_arguments.IgnoreErrors)
+                        throw ex;
+                    else
+                        Console.WriteLine($"An error occured opening '{Path.GetFileName(archivePath)}'. Skipping...");
+                }
 
-                    // Some Console properties might not be available in certain situations, 
-                    // e.g. when redirecting stdout. To prevent crashing, setting the cursor position should only
-                    // be done if there actually is a cursor to be set.
+                int count = 0;
+                int total = archive.Files.Count(x => Filter(x.FullPath));
+                int line = -1;
+                int prevLength = 0;
+
+                // Some Console properties might not be available in certain situations, 
+                // e.g. when redirecting stdout. To prevent crashing, setting the cursor position should only
+                // be done if there actually is a cursor to be set.
+                try
+                {
+                    line = Console.CursorTop;
+                }
+                catch (IOException) { }
+
+                foreach (var entry in archive.Files)
+                {
+                    if (!Filter(entry.FullPath))
+                        continue;
+
+                    string output = $"Extracting: {++count}/{total} - {entry.FullPath}".PadRight(prevLength);
+
+                    if (line > -1)
+                    {
+                        Console.SetCursorPosition(0, line);
+                        Console.Write(output);
+                    }
+                    else
+                    {
+                        Console.WriteLine(output);
+                    }
+                    prevLength = output.Length;
+
                     try
                     {
-                        line = Console.CursorTop;
-                    }
-                    catch (IOException) { }
-
-                    foreach (var entry in archive.Files)
-                    {
-                        if (!Filter(entry.FullPath))
-                            continue;
-
-                        string output = $"Extracting: {++count}/{total} - {entry.FullPath}".PadRight(prevLength);
-
-                        if (line > -1)
-                        {
-                            Console.SetCursorPosition(0, line);
-                            Console.Write(output);
-                        }
-                        else
-                        {
-                            Console.WriteLine(output);
-                        }
-                        prevLength = output.Length;
-
                         entry.Extract(destination, true);
                     }
-
-                    Console.WriteLine();
+                    catch (Exception ex)
+                    {
+                        if (!_arguments.IgnoreErrors)
+                            throw ex;
+                        else
+                            Console.WriteLine($"An error occured extracting '{entry.FullPath}'. Skipping...");
+                    }
                 }
+
+                Console.WriteLine();
             });
         }
 
         static Archive OpenArchive(string file, bool ati)
         {
             Archive archive = null;
+            string extension = Path.GetExtension(file);
 
-            try
+            switch (extension.ToLower())
             {
-                string extension = Path.GetExtension(file);
-
-                switch (extension.ToLower())
-                {
-                    case ".bsa":
-                    case ".dat":
-                        archive = new SharpBSABA2.BSAUtil.BSA(file, _arguments.Encoding);
-                        break;
-                    case ".ba2":
-                        archive = new SharpBSABA2.BA2Util.BA2(file, _arguments.Encoding) { UseATIFourCC = ati };
-                        break;
-                    default:
-                        throw new Exception($"Unrecognized archive file type ({extension}).");
-                }
-
-                archive.Files.Sort((a, b) => string.CompareOrdinal(a.LowerPath, b.LowerPath));
-                return archive;
+                case ".bsa":
+                case ".dat":
+                    archive = new SharpBSABA2.BSAUtil.BSA(file, _arguments.Encoding);
+                    break;
+                case ".ba2":
+                    archive = new SharpBSABA2.BA2Util.BA2(file, _arguments.Encoding) { UseATIFourCC = ati };
+                    break;
+                default:
+                    throw new Exception($"Unrecognized archive file type ({extension}).");
             }
-            catch (Exception ex)
-            {
-                Console.WriteLine("An error occured:");
-                Console.WriteLine(ex.Message);
-                return null;
-            }
+
+            archive.Files.Sort((a, b) => string.CompareOrdinal(a.LowerPath, b.LowerPath));
+            return archive;
         }
 
         static bool Filter(string input)
@@ -291,42 +339,47 @@ namespace BSA_Browser_CLI
             return true;
         }
 
-        private static void PrintFileList(List<string> files, ListOptions options)
+        private static void PrintFileList(List<string> archives, ListOptions options)
         {
-            files.ForEach(file =>
+            archives.ForEach(archivePath =>
             {
-                if (files.Count > 1)
-                    Console.WriteLine($"{Path.GetFileName(file)}:");
+                if (archives.Count > 1)
+                    Console.WriteLine($"{Path.GetFileName(archivePath)}:");
 
-                var archive = OpenArchive(file, false);
+                Archive archive = null;
 
-                if (archive == null)
+                try
                 {
-                    Console.WriteLine("{0}Couldn\'t open archive.", files.Count > 1 ? "\t" : string.Empty);
+                    archive = OpenArchive(archivePath, false);
                 }
-                else
+                catch (Exception ex)
                 {
-                    bool filesize = false;
-                    string prefix = string.Empty;
+                    if (!_arguments.IgnoreErrors)
+                        throw ex;
+                    else
+                        Console.WriteLine($"An error occured opening '{Path.GetFileName(archivePath)}'. Skipping...");
+                }
 
-                    if (options.HasFlag(ListOptions.Archive))
-                        prefix = Path.GetFileName(archive.FullPath);
+                bool filesize = false;
+                string prefix = string.Empty;
 
-                    if (options.HasFlag(ListOptions.FullPath))
-                        prefix = Path.GetFullPath(archive.FullPath);
+                if (options.HasFlag(ListOptions.Archive))
+                    prefix = Path.GetFileName(archive.FullPath);
 
-                    filesize = options.HasFlag(ListOptions.FileSize);
+                if (options.HasFlag(ListOptions.FullPath))
+                    prefix = Path.GetFullPath(archive.FullPath);
 
-                    foreach (var entry in archive.Files)
-                    {
-                        if (!Filter(entry.FullPath))
-                            continue;
+                filesize = options.HasFlag(ListOptions.FileSize);
 
-                        string indent = string.IsNullOrEmpty(prefix) && files.Count > 1 ? "\t" : string.Empty;
-                        string filesizeString = filesize ? entry.RealSize + "\t\t" : string.Empty;
+                foreach (var entry in archive.Files)
+                {
+                    if (!Filter(entry.FullPath))
+                        continue;
 
-                        Console.WriteLine($"{indent}{filesizeString}{Path.Combine(prefix, entry.FullPath)}");
-                    }
+                    string indent = string.IsNullOrEmpty(prefix) && archives.Count > 1 ? "\t" : string.Empty;
+                    string filesizeString = filesize ? entry.RealSize + "\t\t" : string.Empty;
+
+                    Console.WriteLine($"{indent}{filesizeString}{Path.Combine(prefix, entry.FullPath)}");
                 }
 
                 Console.WriteLine();
@@ -337,24 +390,25 @@ namespace BSA_Browser_CLI
         {
             Console.WriteLine("Extract or list files inside .bsa and .ba2 archives.");
             Console.WriteLine();
-            Console.WriteLine("bsab [/e] [/l:[options]] [/ati] [/f [pattern]] [/regex [pattern]] [/enc [encoding]] [FILE] [DESTINATION]");
+            Console.WriteLine("bsab [OPTIONS] FILE [FILE...] [DESTINATION]");
             Console.WriteLine();
-            Console.WriteLine("  /? \t\t Display this help page");
-            Console.WriteLine("  /e \t\t Extract all files");
-            Console.WriteLine("  /l \t\t List all files");
-            Console.WriteLine("    options \t  A \t Prepend each line with archive filename");
-            Console.WriteLine("    \t      \t  F \t Prepend each line with full archive file path");
-            Console.WriteLine("    \t      \t  S \t Display file size");
-            Console.WriteLine("  /f \t\t Simple filtering. Wildcard supported");
-            Console.WriteLine("  /regex \t Regex filtering");
-            Console.WriteLine("  /ati \t\t Use ATI header for textures");
-            Console.WriteLine("  /enc \t\t Set encoding to use");
-            Console.WriteLine("    encodings \t  utf7   \t (Default)");
-            Console.WriteLine("    \t   \t  system \t Use System's default encoding");
-            Console.WriteLine("    \t   \t  ascii");
-            Console.WriteLine("    \t   \t  unicode");
-            Console.WriteLine("    \t   \t  utf32");
-            Console.WriteLine("    \t   \t  utf8");
+            Console.WriteLine("  -h, --help             Display this help page");
+            Console.WriteLine("  -i                     Ignore errors with opening archives or extracting files");
+            Console.WriteLine("  -e                     Extract all files. Options:");
+            Console.WriteLine("  -l:[OPTIONS]           List all files");
+            Console.WriteLine("     options               A   Prepend each line with archive filename");
+            Console.WriteLine("                           F   Prepend each line with full archive file path");
+            Console.WriteLine("                           S   Display file size");
+            Console.WriteLine("  -f FILTER              Simple filtering. Wildcard supported");
+            Console.WriteLine("  --regex REGEX          Regex filtering");
+            Console.WriteLine("  --ati                  Use ATI header for textures");
+            Console.WriteLine("  --encoding ENCODING    Set encoding to use");
+            Console.WriteLine("     encodings             utf7     (Default)");
+            Console.WriteLine("                           system   Use System's default encoding");
+            Console.WriteLine("                           ascii");
+            Console.WriteLine("                           unicode");
+            Console.WriteLine("                           utf32");
+            Console.WriteLine("                           utf8");
             Console.WriteLine();
         }
     }
