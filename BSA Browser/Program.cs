84373abe73937b311e8383affa868a536d9ded19
Alexander Ellingsen <alexellingsen@gmail.com>
2021-03-19 22:50:18 +0100

    Fix missing using

--- BSA Browser/Program.cs
+++ BSA Browser/Program.cs
@@ -4,6 +4,7 @@ using System;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Threading;
 using System.Windows.Forms;
 using MsVB = Microsoft.VisualBasic.ApplicationServices;
 
