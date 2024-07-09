c9bd0ebbd81d9e6f371cb842b41f74e2c8efb9cb
Alexander Ellingsen <alexellingsen@gmail.com>
2024-07-09 17:52:59 +0200

    Avoid converting FullPath to lower when sorting, improving performance a lot

--- BSA Browser/Sorting/ArchiveFileSorter.cs
+++ BSA Browser/Sorting/ArchiveFileSorter.cs
@@ -22,8 +22,9 @@ namespace BSA_Browser.Sorting
                 case ArchiveFileSortOrder.FilePath:
                     if (a.Archive.HasNameTable || a.HadHashTranslated && b.HadHashTranslated)
                     {
-                        return SortingConfig.Descending ? string.CompareOrdinal(a.LowerPath, b.LowerPath) :
-                                      string.CompareOrdinal(b.LowerPath, a.LowerPath);
+                        return SortingConfig.Descending
+                            ? string.Compare(a.FullPath, b.FullPath, StringComparison.OrdinalIgnoreCase)
+                            : string.Compare(b.FullPath, a.FullPath, StringComparison.OrdinalIgnoreCase);
                     }
                     else if (a.HadHashTranslated != b.HadHashTranslated)
                     {
@@ -32,32 +33,37 @@ namespace BSA_Browser.Sorting
                     }
                     else
                     {
-                        return SortingConfig.Descending ? a.Index.CompareTo(b.Index) :
-                                      b.Index.CompareTo(a.Index);
+                        return SortingConfig.Descending
+                            ? a.Index.CompareTo(b.Index)
+                            : b.Index.CompareTo(a.Index);
                     }
 
                 case ArchiveFileSortOrder.FileSize:
-                    return SortingConfig.Descending ? a.DisplaySize.CompareTo(b.DisplaySize) :
-                                  b.DisplaySize.CompareTo(a.DisplaySize);
+                    return SortingConfig.Descending
+                        ? a.DisplaySize.CompareTo(b.DisplaySize)
+                        : b.DisplaySize.CompareTo(a.DisplaySize);
 
                 case ArchiveFileSortOrder.Archive:
                     return SortingConfig.Descending
-                        ? string.CompareOrdinal(a.Archive.FileName.ToLower(), b.Archive.FileName.ToLower())
-                        : string.CompareOrdinal(b.Archive.FileName.ToLower(), a.Archive.FileName.ToLower());
+                        ? string.Compare(a.Archive.FileName, b.Archive.FileName, StringComparison.OrdinalIgnoreCase)
+                        : string.Compare(b.Archive.FileName, a.Archive.FileName, StringComparison.OrdinalIgnoreCase);
 
                 case ArchiveFileSortOrder.Extra:
                     if (a is BA2TextureEntry && b is BA2TextureEntry)
                     {
                         string af = Enum.GetName(typeof(DXGI_FORMAT), (a as BA2TextureEntry).format);
                         string bf = Enum.GetName(typeof(DXGI_FORMAT), (b as BA2TextureEntry).format);
-                        return SortingConfig.Descending ? string.CompareOrdinal(af, bf) :
-                                      string.CompareOrdinal(bf, af);
+
+                        return SortingConfig.Descending
+                            ? string.CompareOrdinal(af, bf)
+                            : string.CompareOrdinal(bf, af);
                     }
                     else
                     {
                         // Sort by file path since Extra will be empty
-                        return SortingConfig.Descending ? string.CompareOrdinal(a.LowerPath, b.LowerPath) :
-                                      string.CompareOrdinal(b.LowerPath, a.LowerPath);
+                        return SortingConfig.Descending
+                            ? string.Compare(a.FullPath, b.FullPath, StringComparison.OrdinalIgnoreCase)
+                            : string.Compare(b.FullPath, a.FullPath, StringComparison.OrdinalIgnoreCase);
                     }
 
                 default:
